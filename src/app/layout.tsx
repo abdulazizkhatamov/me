2dfaa89211e12c12b3df450b080642dd9a937875
abdulazizkhatamov <abdulazizkhatamov@hotmail.com>
2025-07-27 17:16:59 +0500

    keywords and proper padding in projects

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,32 @@ export const metadata: Metadata = {
   title: "Abdulaziz Khatamov | Full Stack Web Developer",
   description:
     "Experienced full stack JavaScript developer specializing in React, Next.js, Node.js, and scalable web solutions.",
+  keywords: [
+    "Abdulaziz Khatamov",
+    "Full Stack Developer",
+    "Web Developer",
+    "JavaScript Developer",
+    "React Developer",
+    "Next.js Developer",
+    "Node.js Developer",
+    "Freelance Developer",
+    "Web Development",
+    "Full Stack JavaScript",
+    "Software Engineer",
+    "Web Applications",
+    "Frontend Developer",
+    "Backend Developer",
+    "UI/UX Developer",
+    "Responsive Design",
+    "Web Design",
+    "Web Development Portfolio",
+    "Web Development Projects",
+    "Web Development Skills",
+    "Web Development Experience",
+    "Web Development Testimonials",
+    "Web Development Contact",
+    "Web Development About Me",
+  ],
 };
 
 export default function RootLayout({
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -28,7 +28,7 @@ export default function Page() {
   };
 
   return (
-    <div className="w-screen min-h-screen">
+    <div className="w-screen min-h-screen py-10 md:py-0">
       <Swiper
         centeredSlides
         autoplay={{
@@ -42,7 +42,7 @@ export default function Page() {
       >
         {projects.map((project) => (
           <SwiperSlide key={project.id}>
-            <div className="w-full min-h-screen bg-cover bg-center relative flex items-center justify-start">
+            <div className="w-full min-h-screen relative flex items-center justify-start">
               <div className="absolute inset-0 backdrop-blur-sm" />
 
               <div className="relative z-10 max-w-7xl mx-auto py-12 px-4 md:px-8 lg:px-0">

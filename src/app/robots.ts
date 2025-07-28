137d0bd9df95253a0fc9f8dfd87aa41eeee575ab
abdulazizkhatamov <abdulazizkhatamov@hotmail.com>
2025-07-28 15:41:13 +0500

    sitemap.xml

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://www.abdulazizkhatamov.com/sitemap.xml",
+    sitemap: "https://www.abdulazizkhatamov.xyz/sitemap.xml",
   };
 }
